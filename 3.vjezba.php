98c4d8e2cf05b2c731477c7d0ae73412eb18efcd
vabiland <vabiland@users.noreply.github.com>
2022-03-27 23:53:17 +0200

    Add files via upload

---
+++ 3.vjezba.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>HTML Forma</h2>
+
+<form method="POST" action="/skripta.php">
+        <select type="text" name="marka">
+                <option>Volvo</option>
+                <option>Saab</option>
+                <option>Fiat</option>
+                <option>Audi</option>
+              </select>
+  <input type="submit" value="Pošalji">
+</form>
+
+</body>
+</html>
---
+++ skripta.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+<body>
+    <?php
+ $marka = $_POST['marka'];
+ echo "Izabrana je marka vozila: $marka";
+
+?>
+</body>
+    </html>
